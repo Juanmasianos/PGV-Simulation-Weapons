9d6242c0416bd3f780f9ca90af51af5a9985bc5a
Juan Manuel <usr3507@salesianos-lacuesta.net>
2025-11-17 20:04:25 +0000

    feat: Added the shared resource

--- src/net/salesianos/militaryzone/ExperimentalShootingRange.java
+++ src/net/salesianos/militaryzone/ExperimentalShootingRange.java
@@ -1,7 +1,24 @@
 package net.salesianos.militaryzone;
 
+import java.util.ArrayList;
+
 public class ExperimentalShootingRange {
     
-    
+    private ArrayList<String> storedWeapons = new ArrayList<>(10);
+
+    public ExperimentalShootingRange() {
+    }
+
+    public void addWeapon(String weapon) {
+
+        this.storedWeapons.add(weapon);
+
+    }
+
+    public void consumeWeapon() {
+
+        this.storedWeapons.remove(0);
+
+    }
 
 }
