255700cf11f35c34558a4974bfd7f62e9cce067f
Juan Manuel <usr3507@salesianos-lacuesta.net>
2025-11-17 18:15:33 +0000

    feat: Added the producer

---
+++ src/net/salesianos/militaryzone/ExperimentalShootingRange.java
@@ -0,0 +1,7 @@
+package net.salesianos.militaryzone;
+
+public class ExperimentalShootingRange {
+    
+    
+
+}
---
+++ src/net/salesianos/militaryzone/WeaponsFactory.java
@@ -0,0 +1,26 @@
+package net.salesianos.militaryzone;
+
+
+public class WeaponsFactory extends Thread{
+    
+    private String id;
+    private int maxWeapons;
+    private String[] weapon = {"laser spartan", "artefacto futurista 009", "trumna prime", "chola de mamá", "gomu gomu no mi", "Terrablade", "Espada de netherita", "pistola nerf", "Excalibur", "Sekki"};
+    private ExperimentalShootingRange shootingRange;
+
+    public WeaponsFactory (String id, int maxWeapons, ExperimentalShootingRange experimentalShootingRange) {
+
+        this.id = id;
+        this.maxWeapons = maxWeapons;
+        this.shootingRange = experimentalShootingRange;
+
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < maxWeapons; i++) {
+            
+        }
+    }
+
+}
